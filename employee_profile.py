c67bc106a576525463230b6e2e6611930c4e6638
Dathatreya <drp.eee208@gmail.com>
2026-06-26 20:07:27 +0530

    Day 2 - Variables, operators and user input

---
+++ employee_profile.py
@@ -0,0 +1,14 @@
+Name = input("Enter your name: ")
+Experience = input("Enter your experience: ")
+Role = input("what is your current role: ")
+Domain = input("Enter your domain: ")
+
+
+print("--------------------")
+print("Employee Profile")
+print("--------------------")
+print("Name         :", Name)
+print("Experience   :", Experience)
+print("Role         :", Role)
+print("Domain       :", Domain)
+print("-----------------------")
