914f320b390b2886b238b38f7437bb05099c8d6b
Dathatreya <drp.eee208@gmail.com>
2026-07-08 22:01:27 +0530

    Folder structure created and moved all the files to respective folders

---
+++ python_learning/01_python_basics/strings.py
@@ -0,0 +1,11 @@
+# Extract from start/end in the string
+patiend_id = "PAT012345"
+
+# print last 
+print(patiend_id[4:])
+# print 4 chars from back in the string
+print(patiend_id[-4:])
+# print 2 chars from the front in the string
+print(patiend_id[:2])
+# print all chars except 2 chars from the back in the string
+print(patiend_id[:-2])
